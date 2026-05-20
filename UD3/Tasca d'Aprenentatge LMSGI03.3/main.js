dacebacb2ee19917ccc574a656e0859ec4af4f11
apwpaucasesnoves <apw@paucasesnovescifp.cat>
2026-05-20 18:20:12 +0200

    Create main.js

---
+++ UD3/Tasca d'Aprenentatge LMSGI03.3/main.js
@@ -0,0 +1,83 @@
+console.log(document.querySelector('h1));
+
+console.log(document.querySelector("h1"));
+
+console.log(document.querySelector(".paragraf"));
+
+
+
+const titol=(document.querySelector("h1"));
+
+titol.textContent = "Titol de JavaScript";
+
+
+
+
+const text=document.getElementsByClassName("paragraf");
+
+console.log(text);
+
+
+
+
+const text=document.getElementById("paragraf");
+
+console.log(text);
+
+
+
+
+const text=document.querySelectorAll("p");
+
+console.log(text);
+
+
+
+
+const text=document.querySelector(".paragraf");
+
+text.innerHTML="Hola";
+
+
+const text=document.querySelector(".paragraf");
+
+
+//text.textContent="<h1>Hola amb texrContent</h1>";
+
+text.innerHTML="<p>Hola amb innerHTML</p>";
+
+console.log(text);
+
+const text=document.querySelector(".paragraf");
+
+text.textContent="<h1>Hola amb texrContent</h1>";
+
+//text.innerHTML="<p>Hola amb innerHTML</p>";
+
+console.log(text);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
